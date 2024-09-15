205dd0a358d581d1986650157e14357d14f43845
CristianRC03 <crizcamach2003@gmail.com>
2024-09-15 02:54:13 -0600

    GitHub workflows

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { MongoDataBase } from './data/init';
+import { envs } from './config/envs';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+(async () => await MongoDataBase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB
+}))();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
